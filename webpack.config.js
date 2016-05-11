e6f6cfca26675126073315534baf0fc5fd65edfb
Robin Millette <robin@millette.info>
2016-05-11 14:00:09 -0400

    Separate npm modules from other projects.

--- webpack.config.js
+++ webpack.config.js
@@ -16,12 +16,38 @@ if (process.env.NODE_ENV === 'production') {
   plugins.push(new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } }))
 }
 
+const modules = [
+  'generator-nm-standard',
+  'rollodeqc-gh-search-users',
+  'rollodeqc-gh-utils',
+  'rollodeqc-gh-user',
+  'rollodeqc-gh-user-events',
+  'rollodeqc-gh-repos',
+  'rollodeqc-gh-bookworm',
+  'rollodeqc-gh-users',
+  'rollodeqc-gh-search-users-all',
+  'rollodeqc-qc-locator',
+  'aurtier',
+  'star-where',
+  'librarian-api',
+  'package-managers'
+]
+
+const modulesMore = [
+  'rollodeqc-dev-profile',
+  'rollodeqc-wassup',
+  'webqcdevs',
+  'meridas',
+  'boy-lost-webpack'
+]
+
 module.exports = {
   entry: [ './entry.js', 'file?name=index.html!jade-html!./index.jade' ],
   output: { path: __dirname, filename: 'bundle.js' },
   devServer: { inline: true, host: '0.0.0.0', port: 1234 },
   module: { loaders: loaders },
   plugins: plugins,
+  jadeLoader: { locals: { modules: modules, modulesMore: modulesMore } },
   postcss: [
     require('postcss-import')({ addDependencyTo: webpack }),
     require('postcss-url')(),
