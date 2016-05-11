10916b6c96916e439bfda64438b8732608cc39b3
Robin Millette <robin@millette.info>
2016-05-11 13:41:14 -0400

    Add babel and uglify on prod.

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
 'use strict'
 
+const webpack = require('webpack')
+
+const loaders = [
+  { test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader' },
+  { test: /\/js\/(main|utils)\.js$/, loader: 'babel?presets[]=es2015' }
+]
+
+const plugins = [ new webpack.ProvidePlugin({
+  'Promise': 'imports?this=>global!exports?global.Promise!es6-promise',
+  'window.fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+}) ]
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } }))
+}
+
 module.exports = {
-  entry: [
-    './entry.js',
-    'file?name=index.html!jade-html!./index.jade'
-  ],
-  output: {
-    path: __dirname,
-    filename: 'bundle.js'
-  },
-  devServer: {
-    inline: true,
-    host: '0.0.0.0',
-    port: 1234
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader!postcss-loader'
-      }
-    ]
-  },
-  postcss: (webpack) => [
+  entry: [ './entry.js', 'file?name=index.html!jade-html!./index.jade' ],
+  output: { path: __dirname, filename: 'bundle.js' },
+  devServer: { inline: true, host: '0.0.0.0', port: 1234 },
+  module: { loaders: loaders },
+  plugins: plugins,
+  postcss: [
     require('postcss-import')({ addDependencyTo: webpack }),
     require('postcss-url')(),
     require('postcss-cssnext')(),
